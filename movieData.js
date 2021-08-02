c03f27e6550a970100b89bf13df70dac4b52b9cd
Maria Quadriq <mquadri23@gmail.com>
2021-08-02 15:52:53 -0500

    added components

--- movieData.js
+++ movieData.js
@@ -1,21 +1,5 @@
 const axios = require("axios");
 
-// var options = {
-//     method: 'GET',
-//     url: 'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt1375666',
-//     headers: {
-//       'x-rapidapi-key': '1954bd9558msha0ca29475ac91b9p1f46e0jsnaafc10cad578',
-//       'x-rapidapi-host': 'imdb-internet-movie-database-unofficial.p.rapidapi.com'
-//     }
-//   };
-  
-//   axios.request(options).then(function (response) {
-//       console.log(response.data);
-//   }).catch(function (error) {
-//       console.error(error);
-//   });
-
-
 var options = {
     method: 'GET',
     url: 'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/spiderman',
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//components
+import Header from './components/Header'
+
+//styles
+import { GlobalStyle } from './GlobalStyle';
+
+const App = () => { 
+  return (
+    <div className="App">
+      <Header />
+      Start here.
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
